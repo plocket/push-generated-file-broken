525198cc13c45135176233e8d4c1985c332b3648
plocket <52798256+plocket@users.noreply.github.com>
2020-10-16 08:42:28 -0400

    Clean up a bit

--- push-generated-file.sh
+++ push-generated-file.sh
@@ -5,7 +5,7 @@
 # - https://github.com/dmnemec/copy_file_to_another_repo_action
 # Also used https://www.shellcheck.net/
 
-# Hope to produce better error feedback.
+# Produce better feedback about errors
 set -e
 set -x
 
@@ -15,20 +15,6 @@ SOURCE_EMAIL="$INPUT_USER_EMAIL"
 # DESTINATION_USERNAME="$5"
 DESTINATION_REPO="$INPUT_DESTINATION_REPO"
 DESTINATION_BRANCH="$INPUT_DESTINATION_BRANCH"
-
-# Destructive
-#    - source-directory }}
-#    - ${{ inputs.destination-github-username }}
-#    - ${{ inputs.destination-repository-name }}
-#    - ${{ inputs.user-email }}
-#    - ${{ inputs.destination-repository-username }}
-#    - ${{ inputs.target-branch }}
-# Non-generating
-#    - ${{ inputs.source-file }}
-#    - ${{ inputs.destination-repo }}
-#    - ${{ inputs.destination-folder }}
-#    - ${{ inputs.user-email }}
-#    - ${{ inputs.destination-branch }}
 
 # Give warnings
 if [ -z "$INPUT_SOURCE_FILE" ]
@@ -58,11 +44,6 @@ if [ -z "$INPUT_DESTINATION_FOLDER" ]
 then
   INPUT_DESTINATION_FOLDER=""
 fi
-
-#if [ -z "$DESTINATION_USERNAME" ]
-#then
-#  DESTINATION_USERNAME="$SOURCE_USERNAME"
-#fi
 if [ -z "$DESTINATION_BRANCH" ]
 then
   DESTINATION_BRANCH="main"
@@ -71,7 +52,6 @@ fi
 CLONE_DIR=$(mktemp -d)
 
 echo "Cloning destination git repository"
-# Setup git
 git config --global user.email "$SOURCE_EMAIL"
 git config --global user.name "$SOURCE_USERNAME"
 git clone --single-branch --branch "$DESTINATION_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$DESTINATION_REPO.git" "$CLONE_DIR"
